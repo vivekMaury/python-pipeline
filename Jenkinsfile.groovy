9e2c7abdca5f5e0baa4435674c5c4c30377d2944
Vivekanand Maurya <55177800+vivekMaury@users.noreply.github.com>
2021-07-09 22:59:57 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,6 +3,10 @@ node() {
     deleteDir()
     checkout scm
   }
+  
+  options {
+  buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '10', numToKeepStr: '10')
+}
   stage('build') {
     bat 'python hello.py'
     bat 'python del_buil_his.py'
