95e47a058c4cb0a44f8707507f5a3f5391d6c160
Vivekanand Maurya <55177800+vivekMaury@users.noreply.github.com>
2021-07-10 00:46:16 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,9 +4,8 @@ pipeline {
         buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')
         }
      stages('First') {
-            steps {
-                echo 'Hello World'
-            }
+        echo 'Hello World'
+         }
        stage('build') {
          bat 'python hello.py'
          bat 'python del_buil_his.py'
